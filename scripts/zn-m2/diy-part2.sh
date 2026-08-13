03c641f406671017aa2f0230184c876ed77a8b46
itlegend-f <itlegend@foxmail.com>
2026-08-13 13:04:01 +0800

    Update diy-part2.sh

--- scripts/zn-m2/diy-part2.sh
+++ scripts/zn-m2/diy-part2.sh
@@ -12,3 +12,23 @@
 #优先安装 passwall 源
 #./scripts/feeds install -a -f -p passwall_packages
 #./scripts/feeds install -a -f -p passwall_luci
+
+#增加集客AC
+git clone --depth=1 https://github.com/openwrt-fork/openwrt-gecoosac.git package/luci-app-gecoosac
+
+# 移除 OpenWrt Feeds 自带的核心库
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
+
+# 移除 OpenWrt Feeds 过时的LuCI版本
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-openclash
+git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall package/luci-app-passwall
+git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall2 package/luci-app-passwall2
+git clone --depth=1 https://github.com/vernesong/OpenClash package/luci-app-openclash
+
+# 清理 PassWall 的 chnlist 规则文件
+# echo "baidu.com"  > package/luci-app-passwall/luci-app-passwall/root/usr/share/passwall/rules/chnlist
+
+./scripts/feeds update -i -a
+./scripts/feeds install -a
