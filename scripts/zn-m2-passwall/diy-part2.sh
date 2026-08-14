d8ce6929f45bd65c0e4daa434e8de77d4533fb72
itlegend-f <itlegend@foxmail.com>
2026-08-15 00:52:09 +0800

    Update diy-part2.sh

--- scripts/zn-m2-passwall/diy-part2.sh
+++ scripts/zn-m2-passwall/diy-part2.sh
@@ -9,6 +9,11 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+
+# 替换golang版本为1.26
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
+
 #优先安装 passwall 源
 ./scripts/feeds install -a -f -p passwall_packages
 ./scripts/feeds install -a -f -p passwall_luci
