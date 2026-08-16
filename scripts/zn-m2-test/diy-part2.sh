9b52d2c7f57071ac95a02d8cf56dc8f3de48928f
itlegend-f <itlegend@foxmail.com>
2026-08-16 21:47:59 +0800

    Update diy-part2.sh

--- scripts/zn-m2-test/diy-part2.sh
+++ scripts/zn-m2-test/diy-part2.sh
@@ -17,7 +17,7 @@
 git clone --depth=1 https://github.com/openwrt-fork/openwrt-gecoosac.git feeds/luci/applications/luci-app-gecoosac
 
 #应用过滤
-git clone https://github.com/destan19/OpenAppFilter.git feeds/luci/applications/OpenAppFilter
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 # 移除 OpenWrt Feeds 自带的核心库
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
