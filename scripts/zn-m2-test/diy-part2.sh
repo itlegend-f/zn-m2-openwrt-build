cf5f39a0be10c1b7fdd6702cd7262551bc7538af
ming1386 <itlegend@foxmail.com>
2026-08-16 17:35:24 +0800

    增加应用过滤

--- scripts/zn-m2-test/diy-part2.sh
+++ scripts/zn-m2-test/diy-part2.sh
@@ -16,6 +16,9 @@ git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/l
 #增加集客AC
 git clone --depth=1 https://github.com/openwrt-fork/openwrt-gecoosac.git feeds/luci/applications/luci-app-gecoosac
 
+#应用过滤
+git clone https://github.com/destan19/OpenAppFilter.git feeds/luci/applications/OpenAppFilter
+
 # 移除 OpenWrt Feeds 自带的核心库
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall-packages feeds/packages/passwall-packages
