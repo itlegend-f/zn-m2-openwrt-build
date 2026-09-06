b43fd48dae620e5f723e35e31f0e7cece3f5bd8c
itlegend-f <itlegend@foxmail.com>
2026-09-07 03:43:32 +0800

    Update diy-part3.sh

--- scripts/LEDE-CMIOT-AX18-test/diy-part3.sh
+++ scripts/LEDE-CMIOT-AX18-test/diy-part3.sh
@@ -14,7 +14,7 @@
 #sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 #修改版本信息
-sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt IPQ6000 ZN-M2 (build time: $(date +%Y%m%d))'/g"  package/base-files/files/etc/openwrt_release
+#sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt IPQ6000 ZN-M2 (build time: $(date +%Y%m%d))'/g"  package/base-files/files/etc/openwrt_release
 
 # ttyd免登陆
 sed -i -r 's#/bin/login#/bin/login -f root#g' feeds/packages/utils/ttyd/files/ttyd.config
